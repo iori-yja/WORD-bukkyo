0a2fb3a64f0a260a46fcaf897436c13aa1ecf13e
Iori YONEJI <fivo.11235813@gmail.com>
2015-05-18 16:58:38 +0900

    felica to SJIS command implemented.

---
+++ hex2bin.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main(void)
+{
+	char input[3];
+	char buf[17];
+	char c;
+
+
+	for (int i = 0; i < 16; i++) {
+		fgets(input, 3, stdin);
+		puts(input);
+		c = 0;
+		c = (char) strtol(input, NULL, 16);
+		buf[i] = c;
+		if (c == 0) break;
+	}
+	buf[16] = 0;
+	puts(buf);
+
+	return 0;
+
+}
+
