72e1c2370874b5a8c3a5e9d1baa706adbb008a78
Iori YONEJI <fivo.11235813@gmail.com>
2015-05-18 17:55:23 +0900

    time-out is implemented.

--- pip.sh
+++ pip.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-username=`felica_dump | grep "0040:0003:" | sed -e "s/^.*0040:0003:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf`
-userid=`felica_dump | grep "0040:0000:" | sed -e "s/^.*0040:0000:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf`
-user=`expr $userid / 100000 - 1000000000`
-echo $username
-echo $user
-beep
-read item
+olduser=""
 
-amount=`./searchitem.sh $item`
-./withdrawal.sh $amount $user
+while true;do
+	while [ ! "$fdump" ]; do
+		fdump=`felica_dump`
+	done
+
+
+	username=`echo "$fdump" | grep "0040:0003:" | sed -e "s/^.*0040:0003:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf`
+	userid=`echo "$fdump" | grep "0040:0000:" | sed -e "s/^.*0040:0000:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf`
+	user=`expr $userid / 100000 - 1000000000`
+
+	if [ "$olduser" != "$user" ]; then
+		echo "Card detected!"
+		echo $username
+		echo $user
+	fi
+
+	olduser=$user
+	read -t 1 item
+
+	if [ $item ];then
+		amount=`./searchitem.sh $item`
+		./withdrawal.sh $amount $user
+		echo "$item, $username"
+		echo "Thank you!"
+	else
+		fdump=`felica_dump`
+		if [ "$fdump" ];then
+			continue;
+		else
+			echo "===Aborted==="
+			olduser=""
+		fi
+	fi
+done
 
