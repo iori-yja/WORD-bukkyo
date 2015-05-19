7ab9150679e271447d681d6cce8522525f2d338d
wordian <wordian@word-ac.net>
2015-05-20 08:06:50 +0900

    ver 2
    - Querying item price is handled. (price and name are shown for 3 sec)
    - Code refactored.
    - Reduce race condition.
    - Parallel processing of polling loop for felica and Barcode reader event listenner.

--- getbalance.sh
+++ getbalance.sh
@@ -4,9 +4,9 @@ USERDB="user.csv"
 current=`grep "$1" $USERDB`
 
 if [ ! $current ]; then
-	echo "残高: 0円"
+	echo "残高: 0 BKD"
 else
 	balance=`echo $current | gawk -v FPAT='([^,]+)|(\"[^\"]+\")' -e '{print $4}'|tail -1`
-	echo "残高: $balance 円"
+	echo "残高: $balance BKD"
 fi
 
--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/zsh
 
-function barcodereader_listener () {
-	item=""
-	read -t 1 item <&p
-	echo $item
-}
-
 function getitemname () {
 	local itemname=`grep $1 item.csv | gawk -v FPAT='([^,]+)|(\"[^\"]+\")' '{print $3}'`
 	if [ $itemname ]; then
@@ -32,6 +26,80 @@ function no_item_found () {
 	kill -USR1 $1
 }
 
+function check_itemprice () {
+	kill -USR2 $1
+	sleep 0.1
+	echo "$3 のお値段は"
+	echo "$2 BKD"
+	echo "です"
+	echo "$3 のお値段は" >&p
+	echo "$2 BKD" >&p
+	echo "です" >&p
+	kill -USR1 $1
+	sleep 3
+}
+
+function try_withdrawal () {
+	price=`./searchitem.sh $item`
+	exit_status=$?
+	if [ $exit_status != 0 ]; then
+		no_item_found $guicpid $item $username
+		item=""
+	else
+		sudo ./withdrawal.sh "$price" "$user" "$username"
+		exit_status=$?
+		echo $exit_status
+		balance=`./getbalance.sh $user`
+		if [ $exit_status = 0 ]; then
+			kill -USR2 $guicpid
+			itemname=`getitemname $item`
+			sleep 0.1
+			echo "$itemname, $username"
+			echo "Thank you!"
+			echo $balance
+			echo "$itemname, $username" >&p
+			echo "Thank you!" >&p
+			echo $balance >&p
+			post_slack "@maririso speech $username が $itemname を買いました"
+			kill -USR1 $guicpid
+			item=""
+		else
+			kill -USR2 $guicpid
+			itemname=`getitemname $item`
+			sleep 0.1
+			echo "$itemname, $username"
+			echo $balance
+			echo "\"金が足りねえぞクソ\" Exception"
+			echo "$itemname, $username" >&p
+			echo "\"金が足りねえぞクソ\" Exception" >&p
+			echo $balance >&p
+			post_slack "@maririso speech $username はお金がなくて $itemname を買えませんでした"
+			kill -USR1 $guicpid
+			item=""
+		fi
+	fi
+}
+
+function barcodereader_listener () {
+	item=""
+	read -t 1 item <&p
+	echo $item
+	if [ $fdump ]; then
+		try_withdrawal
+	else
+		price=`./searchitem.sh $item`
+		exit_status=$?
+		if [ $exit_status != 0 ]; then
+			no_item_found $guicpid $item " "
+			item=""
+		else
+			itemname=`getitemname $item`
+			check_itemprice $guicpid $price $itemname
+		fi
+	fi
+	item=""
+}
+
 olduser=""
 
 coproc "./title.wish" $$
@@ -39,89 +107,47 @@ guicpid=`ps | grep "title.wish" | awk '{print $1}'`
 fdump=""
 echo $guicpid
 
-trap barcodereader_listener USR1
+sleep 1
 
 while true;do
 	# Waiting for felica card
-	while [ ! "$fdump" ]; do
-		fdump=`timeout 2 felica_dump`
-		if [ "$fdump" = "error" ]; then
-			msg="No card Reader found"
-			echo msg >&p
-			echo msg >&p
-			echo msg >&p
-			sleep 120
-		fi
-	done
+	trap barcodereader_listener USR1
+	fdump=`timeout 2 felica_dump`
 
-	username=`echo "$fdump" | grep "0040:0003:" | sed -e "s/^.*0040:0003:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf -Sw`
-	userid=`echo "$fdump" | grep "0040:0000:" | sed -e "s/^.*0040:0000:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf -Sw`
-	user=`expr $userid / 100000 - 1000000000`
-	balance=`./getbalance.sh $user`
+	if [ ! "$fdump" ]; then
+		kill -USR2 $guicpid
+		olduser=""
 
-	if [ "$olduser" != "$user" ]; then
-		kill -USR1 $guicpid
-		echo "Card detected!"
-		echo $username
-		echo $user
-		echo $balance
-		echo $username >&p
-		echo $user >&p
-		echo $balance >&p
-	fi
+	elif [ "$fdump" = "error" ]; then
+		msg="No card Reader found"
+		echo msg >&p
+		echo msg >&p
+		echo msg >&p
+		sleep 120
 
-	olduser=$user
-
-	if [ $item ];then
-		#if barcode is input
-		amount=`./searchitem.sh $item`
-		exit_status=$?
-		if [ $exit_status != 0 ]; then
-			no_item_found $guicpid $item $username
-			item=""
-		else
-			sudo ./withdrawal.sh "$amount" "$user" "$username"
-			exit_status=$?
-			echo $exit_status
-			balance=`./getbalance.sh $user`
-			if [ $exit_status = 0 ]; then
-				kill -USR2 $guicpid
-				itemname=`getitemname $item`
-				sleep 0.1
-				echo "$itemname, $username"
-				echo "Thank you!"
-				echo $balance
-				echo "$itemname, $username" >&p
-				post_slack "@maririso speech $username が $itemname を買いました"
-				echo "Thank you!" >&p
-				echo $balance >&p
-				kill -USR1 $guicpid
-				item=""
-			else
-				kill -USR2 $guicpid
-				itemname=`getitemname $item`
-				sleep 0.1
-				echo "$itemname, $username"
-				echo $balance
-				echo "\"金が足りねえぞクソ\" Exception"
-				echo "$itemname, $username" >&p
-				echo "\"金が足りねえぞクソ\" Exception" >&p
-				post_slack "@maririso speech $username はお金がなくて $itemname を買えませんでした"
-				echo $balance >&p
-				kill -USR1 $guicpid
-				item=""
-			fi
-		fi
-		#no barcode is input
 	else
-		fdump=`timeout 2 felica_dump`
-		if [ "$fdump" ];then
-			continue;
-		else
-			kill -USR2 $guicpid
-			echo "===Aborted==="
-			olduser=""
+		dummy_username=`echo "$fdump" | grep "0040:0003:" | sed -e "s/^.*0040:0003:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf -Sw`
+		dummy_userid=`echo "$fdump" | grep "0040:0000:" | sed -e "s/^.*0040:0000:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf -Sw`
+		dummy_user=`expr $dummy_userid / 100000 - 1000000000`
+		dummy_balance=`./getbalance.sh $dummy_user`
+
+		username=$dummy_username
+		userid=$dummy_userid
+		user=$dummy_user
+		balance=$dummy_balance
+
+		if [ "$olduser" != "$user" ]; then
+			kill -USR1 $guicpid
+			echo "Card detected!"
+			echo $username
+			echo $user
+			echo $balance
+			echo $username >&p
+			echo $user >&p
+			echo $balance >&p
+			olduser=$user
 		fi
 	fi
+
 done
 
