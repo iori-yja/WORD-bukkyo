473539596e7df280ec1448294b9769b6116454dd
wordian <wordian@word-ac.net>
2015-05-22 15:45:24 +0900

    Revert "Revert "exchange `expr` for bas variable-calculate function""
    This reverts commit 10216f3a330a9024612bb209e40aa17dc24589bd.
    
    Conflicts:
    	run.sh

--- run.sh
+++ run.sh
@@ -6,58 +6,59 @@ olduser=""
 
 killall -9 title.wish
 coproc "./title.wish" $$
-guicpid=`ps | grep "title.wish" | awk '{print $1}' | head -1`
+guicpid=$(pgrep "title.wish" | awk '{print $1}' | head -1)
 
 fdump=""
-echo $guicpid
+echo "${guicpid}"
 
-slackurl=`grep "slackurl:" ../bukkyo.conf| head -1 |awk '{print $2}'`
+slackurl=$(grep "slackurl:" ../bukkyo.conf| head -1 |awk '{print $2}')
 trap barcode_listener USR1
 
 sleep 1
 
 while true;do
 	# Waiting for felica card
-	fdump=`timeout 2 felica_dump`
+	fdump=$(timeout 2 felica_dump)
 
-	if [ ! "$fdump" ]; then
-		if [ "$olduser" ]; then
+	if [ ! "${fdump}" ]; then
+		if [ "${olduser}" ]; then
 			olduser=""
 			sleep 0.3
-			kill -USR2 $guicpid
+			kill -USR2 ${guicpid}
+			kill -USR2 "${guicpid}"
 		fi
 
-	elif [ "$fdump" = "error" ]; then
+	elif [ "${fdump}" = "error" ]; then
 		msg="No card Reader found"
-		kill -USR2 $guicpid
+		kill -USR2 "${guicpid}"
 		sleep 0.1
-		echo $msg >&p
-		echo $msg >&p
-		echo $msg >&p
-		kill -USR1 $guicpid
+		echo "${msg}" >&p
+		echo "${msg}" >&p
+		echo "${msg}" >&p
+		kill -USR1 "${guicpid}"
 		sleep 10
 
 	else
-		dummy_username=`echo "$fdump" | grep "0040:0003:" | sed -e "s/^.*0040:0003:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf -Sw`
-		dummy_userid=`echo "$fdump" | grep "0040:0000:" | sed -e "s/^.*0040:0000:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf -Sw`
-		dummy_user=`expr $dummy_userid / 100000 - 1000000000`
-		dummy_balance=`get_balance $dummy_user`
+		dummy_username=$(echo "${fdump}" | grep "0040:0003:" | sed -e "s/^.*0040:0003:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf -Sw)
+		dummy_userid=$(echo "${fdump}" | grep "0040:0000:" | sed -e "s/^.*0040:0000:\([A-Z,0-9]*\)$/\1/"|./hex2bin|nkf -Sw)
+		dummy_user=$((dummy_userid / 100000 - 1000000000))
+		dummy_balance=$(get_balance "${dummy_user}")
 
-		username=$dummy_username
-		userid=$dummy_userid
-		user=$dummy_user
-		balance=$dummy_balance
+		username="${dummy_username}"
+		userid="${dummy_userid}"
+		user="${dummy_user}"
+		balance="${dummy_balance}"
 
-		if [ "$olduser" != "$user" ]; then
+		if [ "${olduser}" != "${user}" ]; then
 			echo "Card detected!"
-			echo $username
-			echo $user
-			echo $balance
-			echo $username >&p
-			echo $user >&p
-			echo $balance >&p
-			kill -USR1 $guicpid
-			olduser=$user
+			echo "${username}"
+			echo "${user}"
+			echo "${balance}"
+			echo "${username}" >&p
+			echo "${user}" >&p
+			echo "${balance}" >&p
+			kill -USR1 "${guicpid}"
+			olduser="${user}"
 		fi
 	fi
 
