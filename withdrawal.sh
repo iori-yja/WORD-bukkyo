fe0191d12613173e750d3fd3c9ada743eead5933
Iori YONEJI <fivo.11235813@gmail.com>
2015-05-22 13:42:48 +0900

    Revert "surround command with $() instead backquote"
    This reverts commit 733932526cd56a89c53e7a43e27f40b6d0e9f4bd.

--- withdrawal.sh
+++ withdrawal.sh
@@ -1,34 +1,32 @@
-# shellcheck disable=SC1071
 #!/bin/zsh
 
 # comannd amount user
 function add_user () {
-	newid=$(tail -1 "${USERDB}"|gawk -v FPAT='([^,]+)|(\"[^\"]+\")' -e '{print $1}')
-	echo "${newid}"
-	newid=$((newid + 1))
-	echo "${newid},${1},\"${2}\",0,0,0,0" >> "${USERDB}"
+	newid=`tail -1 $USERDB|gawk -v FPAT='([^,]+)|(\"[^\"]+\")' -e '{print $1}'`
+	echo "$newid"
+	newid=`expr $newid + 1`
+	echo "$newid,$1,\"$2\",0,0,0,0" >> $USERDB
 }
 
 USERDB="user.csv"
-current=$(grep "${2}" "${USERDB}")
+current=`grep "$2" $USERDB`
 AWKOPT=""
 
-echo "${current}"
-if [ ! "${current}" ]; then
-	add_user "${2}" "${3}"
-	current=$(grep "${2}" "${USERDB}")
+echo $current
+if [ ! $current ]; then
+	add_user $2 "$3"
+	current=`grep "$2" $USERDB`
 fi
 
-balance=$(echo "${current}" | gawk -v FPAT='([^,]+)|(\"[^\"]+\")' -e '{print $4}')
-echo "${balance}"
-price="${1}"
-newbalance=$((balance - price))
-echo "${newbalance}"
+balance=`echo $current | gawk -v FPAT='([^,]+)|(\"[^\"]+\")' -e '{print $4}'`
+echo $balance
+newbalance=`expr $balance - \( $1 \)`
+echo $newbalance
 
-if [ "${newbalance}" -ge 0 ]; then
-	next=$(echo "${current}" | gawk -v FPAT='([^,]+)|(\"[^\"]+\")' -e '{print $1 "," $2 "," $3 ",'${newbalance}'," $5 "," $6 "," $7 }')
-	echo "$(date) : s/${current}/${next}/" >> sed.log
-	sed -i -e "s/${current}/${next}/" user.csv
+if [ $newbalance -ge 0 ]; then
+	next=`echo $current | gawk -v FPAT='([^,]+)|(\"[^\"]+\")' -e '{print $1 "," $2 "," $3 ",'$newbalance'," $5 "," $6 "," $7 }'`
+	echo `date` ": s/"$current"/"$next"/" >> sed.log
+	sed -i -e "s/"$current"/"$next"/" user.csv
 	exit 0
 else
 	exit 1
